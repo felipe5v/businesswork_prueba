81fef156c35af2989e5ea84e881765510f34fff7
Fernan1122 <60754935+Fernan1122@users.noreply.github.com>
2020-12-20 01:51:46 -0500

    problemas tecnicos parcero

--- main.py
+++ main.py
@@ -42,14 +42,6 @@ async def auth_user(userAuth: schemas.UserIn, db: Session = Depends(get_db)):
         raise HTTPException(status_code=403, detail="Error de autenticacion")
 
     return  {"Autenticado": True}
-
-@app.post("/users/", response_model=schemas.User)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_username(db, username=user.username)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Username no disponible")
-    return crud.create_user(db=db, user=user)
-
 ##############
 
 @app.post("/users/", response_model=schemas.User)
